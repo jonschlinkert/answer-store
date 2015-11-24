dab0405a11e6d0c8a893411bd77256e67fabc555
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-24 03:47:15 -0500

    first release

--- index.js
+++ index.js
@@ -8,75 +8,157 @@
 'use strict';
 
 var path = require('path');
-var set = require('set-value');
-var get = require('get-value');
 var utils = require('./utils');
 
 /**
  * Create new `Answer` store `name`, with the given `options`.
  *
- * @param {String} `key` The answer key
+ * @param {String} `name` The answer property name.
  * @param {Object} `options` Store options
  * @api public
  */
 
-function Answer(key, options) {
-  if (typeof key !== 'string') {
+function Answer(name, options) {
+  if (typeof name !== 'string') {
     throw new TypeError('expected the first argument to be a string');
   }
+  this.cache = {};
   this.options = options || {};
-  this.name = key;
+  this.name = name;
   this.data = utils.readJson(this.path);
 }
 
 /**
- * Non-emumerable paths cache
- */
-
-Answer.prototype.paths = {};
-
-/**
- * Update the answer.
+ * Store the specified `value` for the current (or given) local, at the current cwd.
  *
  * ```js
  * answer.set('foo');
  * ```
- *
+ * @param {any} `value` The answer value.
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
  * @api public
  */
 
-Answer.prototype.set = function(val) {
-  set(this.data, [this.name, this.cwd], val);
+Answer.prototype.set = function(val, locale) {
+  utils.set(this.data, this.toKey(locale), val);
   this.save();
   return this;
 };
 
 /**
- * Get the answer.
+ * Get the stored answer for the current (or given) `locale` at the current `cwd`.
  *
  * ```js
- * answer.get();
+ * answer.get(locale);
  * ```
- *
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
  * @api public
  */
 
-Answer.prototype.get = function() {
-  return get(this.data, [this.name, this.cwd]);
+Answer.prototype.get = function(locale) {
+  return utils.get(this.data, this.toKey(locale));
 };
 
 /**
- * Delete the entire answer store.
+ * Return true if an answer has been stored for the current
+ * (or given) locale at the working directory.
+ *
+ * ```js
+ * answer.has('foo');
+ * ```
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
+ * @api public
+ */
+
+Answer.prototype.has = function(locale) {
+  return utils.has(this.data, this.toKey(locale));
+};
+
+/**
+ * Set the default answer for the currently defined `locale`.
+ *
+ * ```js
+ * answer.setDefault('foo');
+ * ```
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
+ * @api public
+ */
+
+Answer.prototype.setDefault = function(val, locale) {
+  utils.set(this.data, this.defaultKey(locale), val);
+  this.save();
+  return this;
+};
+
+/**
+ * Get the default answer for the currently defined `locale`.
+ *
+ * ```js
+ * answer.getDefault();
+ * ```
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
+ * @api public
+ */
+
+Answer.prototype.getDefault = function(locale) {
+  return utils.get(this.data, this.defaultKey(locale));
+};
+
+/**
+ * Return true if a value is stored for the current (or given) locale, at the current cwd.
+ *
+ * ```js
+ * answer.hasDefault(locale);
+ * ```
+ * @param {String} `locale` Optionally pass the locale to use, otherwise the default locale is used.
+ * @api public
+ */
+
+Answer.prototype.hasDefault = function(locale) {
+  return utils.has(this.data, this.defaultKey(locale));
+};
+
+/**
+ * Delete the stored values for the current (or given) locale.
+ *
+ * ```js
+ * answer.deleteDefault(locale);
+ * ```
+ * @param {String} `locale` Optionally pass the local to delete.
+ * @api public
+ */
+
+Answer.prototype.deleteDefault = function(locale) {
+  utils.unset(this.data, this.defaultKey(locale));
+  this.save();
+};
+
+/**
+ * Delete the stored values for the current (or given) locale, at the current cwd.
+ *
+ * ```js
+ * answer.del(locale);
+ * ```
+ * @param {String} `locale` Optionally pass the local to delete.
+ * @api public
+ */
+
+Answer.prototype.del = function(locale) {
+  utils.unset(this.data, this.toKey(locale));
+  this.save();
+};
+
+/**
+ * Erase all stored values and delete the answer store from the file system.
  *
  * ```js
  * answer.del();
  * ```
- * @param {Function} `callback`
  * @api public
  */
 
-Answer.prototype.del = function() {
-  this.data[this.name] = null;
+Answer.prototype.erase = function() {
+  this.data = {};
   utils.del.sync(this.path);
 };
 
@@ -86,12 +168,28 @@ Answer.prototype.del = function() {
  * ```js
  * answer.save();
  * ```
- *
- * @api public
  */
 
 Answer.prototype.save = function() {
   utils.writeJson.sync(this.path, this.data);
+};
+
+/**
+ * Create the property key to use for getting and setting
+ * the `default` value for the current locale.
+ */
+
+Answer.prototype.defaultKey = function(locale) {
+  return (locale || this.locale) + '.default';
+};
+
+/**
+ * Create the property key to use for getting and setting
+ * values for the current locale and cwd.
+ */
+
+Answer.prototype.toKey = function(locale) {
+  return utils.toKey(locale || this.locale, this.cwd);
 };
 
 /**
@@ -100,14 +198,14 @@ Answer.prototype.save = function() {
 
 Object.defineProperty(Answer.prototype, 'cwd', {
   set: function(cwd) {
-    this.paths.cwd = cwd;
+    this.cache.cwd = cwd;
   },
   get: function() {
-    if (this.paths.hasOwnProperty('cwd')) {
-      return this.paths.cwd;
+    if (this.cache.cwd) {
+      return this.cache.cwd;
     }
-    var cwd = path.resolve(this.options.cwd || process.cwd());
-    return (this.paths.cwd = cwd);
+    var cwd = this.options.cwd || process.cwd();
+    return (this.cache.cwd = cwd);
   }
 });
 
@@ -117,14 +215,14 @@ Object.defineProperty(Answer.prototype, 'cwd', {
 
 Object.defineProperty(Answer.prototype, 'dest', {
   set: function(dest) {
-    this.paths.dest = dest;
+    this.cache.dest = dest;
   },
   get: function() {
-    if (this.paths.hasOwnProperty('dest')) {
-      return this.paths.dest;
+    if (this.cache.dest) {
+      return this.cache.dest;
     }
     var dest = utils.resolveDir(this.options.dest || '~/answers');
-    return (this.paths.dest = dest);
+    return (this.cache.dest = dest);
   }
 });
 
@@ -134,14 +232,27 @@ Object.defineProperty(Answer.prototype, 'dest', {
 
 Object.defineProperty(Answer.prototype, 'path', {
   set: function(fp) {
-    this.paths.path = fp;
+    this.cache.path = fp;
   },
   get: function() {
-    if (this.paths.hasOwnProperty('path')) {
-      return this.paths.path;
+    if (this.cache.path) {
+      return this.cache.path;
     }
     var fp = path.resolve(this.dest, this.name + '.json');
-    return (this.paths.path = fp);
+    return (this.cache.path = fp);
+  }
+});
+
+/**
+ * Getter/setter for answer path
+ */
+
+Object.defineProperty(Answer.prototype, 'locale', {
+  set: function(locale) {
+    this.cache.locale = locale;
+  },
+  get: function() {
+    return this.cache.locale || this.options.locale || 'en';
   }
 });
 
